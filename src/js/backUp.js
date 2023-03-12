9b04b6488876e73df314dcb25e1d5d12c9d80081
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-03-13 01:55:24 +0200

    js modal doudle v2

--- src/js/backUp.js
+++ src/js/backUp.js
@@ -3,8 +3,6 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-
-
     /* Записываем в переменные массив элементов-кнопок и подложку.
        Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
     var modalButtons = document.querySelectorAll('.js-open-modal'),
@@ -21,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
             /* Предотвращаем стандартное действие элемента. Так как кнопку разные
                люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
                Нужно подстраховаться. */
-            // e.preventDefault();
+            e.preventDefault();
 
             /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
                и будем искать модальное окно с таким же атрибутом. */
@@ -43,14 +41,8 @@ document.addEventListener('DOMContentLoaded', function () {
         item.addEventListener('click', function (e) {
             var parentModal = this.closest('.modal');
 
-
-            if (this.getAttribute('data-modal') != 2) {
-                overlay.classList.remove('active');
-            }
-            console.log(parentModal.getAttribute('data-modal'))
-
             parentModal.classList.remove('active');
-
+            overlay.classList.remove('active');
         });
 
     }); // end foreach
