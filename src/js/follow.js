4b943472b7e44430aa63bc1471407d7cba3d8d90
mrcolti4 <kbulygin456@gmail.com>
2023-03-12 14:46:24 +0200

    Creat: connect slick.js and modal.js files

--- src/js/follow.js
+++ src/js/follow.js
@@ -1,1 +1,13 @@
-console.log(126);
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]")
+  };
+    // console.log(refs);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
